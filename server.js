5c4a7d8031d97fdd03f95f233e6179145ffc0140
MaribelNavia <maribelnavia21@gmail.com>
2018-05-08 01:17:31 -0500

    Passport JWT Authentication Strategy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -24,8 +26,11 @@ mongoose
 	.catch(err => console.log(err));
 
 
+// Passport middleware
+app.use(passport.initialize());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Passport Config 
+require('./config/passport')(passport);
 
 // Use routes 
 app.use('/api/users', users)
