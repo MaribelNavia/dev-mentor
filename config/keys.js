457c8e5da4d68802958318edede43701b8bc5a08
MaribelNavia <maribelnavia21@gmail.com>
2018-05-07 23:06:25 -0500

    Create The JWT

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-	mongoURI: 'mongodb://maribelnavia:mlnmln22@ds115350.mlab.com:15350/dev-mentor'
+	mongoURI: 'mongodb://maribelnavia:mlnmln22@ds115350.mlab.com:15350/dev-mentor',
+	secretOrKey: 'secret'
 };
 
 	
